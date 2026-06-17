2c416e8dbf06e13693999f24df62a5f04f5c4882
Umang <soyumang55@gmail.com>
2026-06-17 19:52:13 +0530

    first commit

---
+++ src/main/java/com/umang/analytics_api/AnalyticsApiApplication.java
@@ -0,0 +1,13 @@
+package com.umang.analytics_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/umang/analytics_api/config/ClickHouseConfig.java
@@ -0,0 +1,25 @@
+package com.umang.analyticsapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+@Configuration
+public class ClickHouseConfig {
+
+    @Bean
+    public Connection clickHouseConnection() throws Exception {
+
+        String url = "jdbc:clickhouse://localhost:8123/analytics";
+
+        Properties props = new Properties();
+
+        props.setProperty("user", "event_user");
+        props.setProperty("password", "event123");
+
+        return DriverManager.getConnection(url, props);
+    }
+}
